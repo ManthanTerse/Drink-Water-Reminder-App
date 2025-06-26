d711beada5ceb4344d9547f5bc1d3bcf1d5e5025
Manthan Terse <tersemanthan2006@gmail.com>
2025-06-26 17:15:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+#Project 04 : Drink Water Reminder 
+
+from flask import Flask, render_template
+import time
+from plyer import notification
+
+
+while(True):
+    print("Please sip some water!")
+    notification.notify(title="Drink Water",
+             message="You have to take a sip of water")
+    time.sleep(60)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
